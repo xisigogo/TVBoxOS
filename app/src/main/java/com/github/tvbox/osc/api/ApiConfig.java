9f039df1c84ed20a529894f0170410b06be535de
xisigogo <125654847+xisigogo@users.noreply.github.com>
2023-03-31 01:32:58 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://52bsj.vip:81/api/v3/file/get/119163/fj.txt?sign=cW8VYaOJrGoSKf_UznjCr9zxok-kOb0olKpfwYbM5D0%3D%3A0");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
